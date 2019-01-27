144a8058106ee978cbc7c6e22cbe18ec4721c768
Shrikant Khandare <shrikant1712@gmail.com>
2019-01-27 16:54:48 +0530

    Added following changes  -- Added CRUD operations for Hotel model  -- Added seeded data for Hotel model

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,35 @@
 class HotelsController < ApplicationController
+  def index
+    @hotels = Hotel.all
+    json_response(@hotels)
+  end
+
+  def create
+    @hotel = Hotel.create!(hotel_params)
+    json_response(@hotel, :created)
+  end
+
+  def show
+    json_response(@hotel)
+  end
+
+  def update
+    @hotel.update(hotel_params)
+    head :no_content
+  end
+
+  def destroy
+    @hotel.destroy
+    head :no_content
+  end
+
+  private
+
+  def hotel_params
+    params.permit(:name, :email, :phone)
+  end
+
+  def set_hotel
+    @hotel = Hotel.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Hotel.create([
+  { name: 'D Place', email: 'd_palace@test.com', phone: '0000000000' },
+  { name: 'BlueWater', email: 'blue_water@test.com', phone: '1111111111' },
+  { name: 'Absolute Barbecues', email: 'abs@test.com', phone: '2222222222' },
+  { name: 'Le Flamington', email: 'las@test.com', phone: '3333333333' },
+  { name: 'Royal China', email: 'royal_china@test.com', phone: '4444444444' },
+  { name: 'The French Window Patisserie', email: 'french_window@test.com', phone: '5555555555' },
+  { name: 'The Sassy Spoon', email: 'sasy_spoon@test.com', phone: '6666666666' },
+  { name: 'Cafe 1730', email: 'cafe_1730@test.com', phone: '7777777777' },
+  { name: 'Vohuman Cafe', email: 'vohuman_cafe@test.com', phone: '8888888888' },
+  { name: 'Royal Palcae', email: 'royal_place@test.com', phone: '9999999999' }
+])
